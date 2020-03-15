100cf771fa37a9943dc98e7e3a88db93131ed0f6
Ubuntu <tianyuan@v100--10.ff4m0t3hepuulmonwl1gxk4ojc.jx.internal.cloudapp.net>
2020-03-15 06:04:45 +0000

    correct mnli

--- scripts/ft_glue.py
+++ scripts/ft_glue.py
@@ -49,25 +49,48 @@ if __name__ == '__main__':
 
 
 
-    cmds =["CUDA_VISIBLE_DEVICES={}".format(args.d),
-           " python3 train.py ~/data/glue-32768-fast/{}/ ".format(dataset_dir_name[index]),
-    "--restore-file {} ".format(args.p),
-    "--max-positions 512 --max-sentences {} ".format(max_sentences[index]),
-    "--max-tokens 4400 --task sentence_prediction --reset-optimizer ",
-    "--reset-dataloader --reset-meters --required-batch-size-multiple 1 \
-    --init-token 0 --separator-token 2 --truncated-sentences ",
-    "--arch {} ".format(args.arch),
-    "--criterion sentence_prediction ",
-    "--num-classes {} ".format(num_classes[index]),
-    "--dropout 0.1 --attention-dropout 0.1 --weight-decay 0.1 \
-    --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-06 \
-    --clip-norm 0.0 --lr-scheduler polynomial_decay ",
-    "--lr {} --total-num-update {} ".format(lrs[index], total_num_updates[index]),
-    "--warmup-updates {} --seed {} ".format(warm_updates[index], rand_int),
-    "--max-epoch 10 \
-    --find-unused-parameters \
-    --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric;"
-           ]
+    if args.task == 'MNLI':
+        cmds =["CUDA_VISIBLE_DEVICES={}".format(args.d),
+               " python3 train.py ~/data/glue-32768-fast/{}/ ".format(dataset_dir_name[index]),
+        "--restore-file {} ".format(args.p),
+        "--max-positions 512 --max-sentences {} ".format(max_sentences[index]),
+        "--max-tokens 4400 --task sentence_prediction --reset-optimizer --valid-subset valid,valid1 ",
+        "--reset-dataloader --reset-meters --required-batch-size-multiple 1 \
+        --init-token 0 --separator-token 2 --truncate-sequence ",
+        "--arch {} ".format(args.arch),
+        "--criterion sentence_prediction ",
+        "--num-classes {} ".format(num_classes[index]),
+        "--dropout 0.1 --attention-dropout 0.1 --weight-decay 0.1 \
+        --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-06 \
+        --clip-norm 0.0 --lr-scheduler polynomial_decay ",
+        "--lr {} --total-num-update {} ".format(lrs[index], total_num_updates[index]),
+        "--warmup-updates {} --seed {} ".format(warm_updates[index], rand_int),
+        "--max-epoch 10 \
+        --find-unused-parameters \
+        --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric;"
+               ]
+
+    else:
+
+        cmds =["CUDA_VISIBLE_DEVICES={}".format(args.d),
+               " python3 train.py ~/data/glue-32768-fast/{}/ ".format(dataset_dir_name[index]),
+        "--restore-file {} ".format(args.p),
+        "--max-positions 512 --max-sentences {} ".format(max_sentences[index]),
+        "--max-tokens 4400 --task sentence_prediction --reset-optimizer ",
+        "--reset-dataloader --reset-meters --required-batch-size-multiple 1 \
+        --init-token 0 --separator-token 2 --truncate-sequence ",
+        "--arch {} ".format(args.arch),
+        "--criterion sentence_prediction ",
+        "--num-classes {} ".format(num_classes[index]),
+        "--dropout 0.1 --attention-dropout 0.1 --weight-decay 0.1 \
+        --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-06 \
+        --clip-norm 0.0 --lr-scheduler polynomial_decay ",
+        "--lr {} --total-num-update {} ".format(lrs[index], total_num_updates[index]),
+        "--warmup-updates {} --seed {} ".format(warm_updates[index], rand_int),
+        "--max-epoch 10 \
+        --find-unused-parameters \
+        --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric;"
+               ]
 
     cmd = ' '
 
